7b171cdfb231cb82e44b8501f3db84be98d75205
Wyatt <s179053@students.aie.edu.au>
2017-10-17 17:49:23 -0700

    more matrixes

--- X-SFW_test/main.cpp
+++ X-SFW_test/main.cpp
@@ -2,20 +2,28 @@
 #include <ctime>
 #include "sfwdraw.h"
 #include "Player.h"
+#include "Transform.h"
 
 int main()
 {
 	sfw::initContext(800, 600);
 	std::srand(std::time(0));
-
-	Player me;
-	me.pos = { 400, 300};
-	me.speed = 3;
+	float t = 0;
+	Transform myT;
+	myT.position = vec2{ 400, 300 };
+	myT.dimension = vec2{ 1,1 };
 	while (sfw::stepContext())
 	{
-		me.update();
-		me.draw();
+		DrawMatrix(myT.getLocalTransform(), 40);
+		myT.angle += 1;
+		if (myT.angle > 90)
+		{
+			myT.dimension = vec2{ sinf(t) + 2, sinf(t) + 2 };
+		}
+		else if (myT.angle < 90)
+		{
+			myT.dimension = vec2{ sinf(t) - 2, sinf(t) - 2 };
+		}
 	}
-
-	sfw::termContext();
+		sfw::termContext();
 }
